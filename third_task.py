e5b8b0a3d9930a1a4f91fa05f7fa4cb66ba0880d
Zhemerenko_Andrii <andr20051@gmail.com>
2023-07-25 14:35:05 +0300

    first_part

--- third_task.py
+++ third_task.py
@@ -2,36 +2,48 @@ import logging
 import argparse
 import csv
 import requests
+from pprint import pprint
 
 
 logging.basicConfig(filename='file.log',
                     level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 
-def explore_data(url_, filename):
+def explore_data(url_, params, filename):
     logging.info('Exploring the data provider site  and download a csv file with users ')
-    response = requests.get(url_)
-    if requests.status_codes == 200:
-        with open(filename + '.csv', 'w', newline='') as file:
-           file.write(response.content)
+    response = requests.get(url_, params=params)
+    if response.status_code == 200:
+        data = response.text
+        with open(filename + '.csv', 'w', newline='', encoding='utf-8') as file:
+            file.write(data)
 
-
-
+def read_data(filename):
+    data = []
+    with open(f'{filename}.csv', 'r', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file, delimiter=',', quotechar='\'')
+        for row in csv_reader:
+            data.append(row)
+    return data
 
 
 if __name__ == '__main__':
-    url = 'https://randomuser.me/api/'
+    url = 'https://randomuser.me/api'
+    params = {
+        'results': 5000,
+        'format': 'csv'
+    }
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--destination_folder', required=True)
-    parser.add_argument('--filename', default='output')
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--destination_folder', required=True)
+    # parser.add_argument('--filename', default='output')
+    #
+    # group = parser.add_mutually_exclusive_group()
+    # group.add_argument('--gender')
+    # group.add_argument('--rows', type=int)
+    # parser.add_argument('log_level')
+    # args = parser.parse_args()
 
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--gender')
-    group.add_argument('--rows', type=int)
-    parser.add_argument('log_level')
-    args = parser.parse_args()
-
-    explore_data(url, args.filename)
-
+    # args.filename
+    explore_data(url, params, 'down_data')
+    pprint(read_data('down_data'))
 
